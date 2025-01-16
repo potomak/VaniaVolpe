a056b1e451c02cd74ebe38590735a79040bbd88b
Giovanni Cappellotto <potomak84@gmail.com>
2025-01-16 08:59:38 -0500

    Define GameObject type

--- sdlexample/example.h
+++ sdlexample/example.h
@@ -11,14 +11,14 @@
 #import "scene.h"
 
 // Declare two game objects for the ball and the paddle
-struct game_object {
+typedef struct game_object {
   float x;
   float y;
   float width;
   float height;
   float vel_x;
   float vel_y;
-};
+} GameObject;
 
 extern Scene example_scene;
 
