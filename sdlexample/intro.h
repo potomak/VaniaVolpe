7052f2fefe25e24cd9f9504ed37592ee4ac66be7
Giovanni Cappellotto <potomak84@gmail.com>
2025-01-15 22:35:54 -0500

    Declare and implement functions to initialize and deinitialize scenes

--- sdlexample/intro.h
+++ sdlexample/intro.h
@@ -8,6 +8,8 @@
 #ifndef intro_h
 #define intro_h
 
+void intro_init(void);
+
 bool intro_load_media(SDL_Renderer *renderer);
 
 void intro_process_input(void);
@@ -15,5 +17,7 @@ void intro_process_input(void);
 void intro_update(void);
 
 void intro_render(SDL_Renderer *renderer);
+
+void intro_deinit(void);
 
 #endif /* intro_h */
